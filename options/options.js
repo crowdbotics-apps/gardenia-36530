ab35dc0fb72e1debd3689b63be9aa7a73aac4de3
Crowdbotics <team@crowdbotics.com>
2022-07-31 18:51:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gardenia_36530",
+  url: "https://gardenia_36530.botics.co",
+  api: "https://gardenia_36530.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
